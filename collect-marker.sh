2c0bf6d2dfe06bde8366a408dd860474da43e0cc
Masao YOKOYAMA <masao.yokoyama@gmail.com>
2010-06-21 00:41:31 -0700

    高速化

--- collect-marker.sh
+++ collect-marker.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
 
-output=analyze.log
-#output=debug.log
-
-node header.js >$output
-
-for file in /mnt/share/txt/*.txt
-#for file in *.txt
-do
-  node collect-marker.js "$file" utf8 >>$output
-done
+node collect-marker.js analyze.log ../txt/ utf8
+#node collect-marker.js debug.log ./ utf8
